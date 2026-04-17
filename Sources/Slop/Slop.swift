04a8c94d06a27aa3adb3fc6a40c05013a3e4aac8
subjectivelyobjective <31091156+subjectivelyobjective@users.noreply.github.com>
2026-04-17 00:52:09 -0400

    Update Slop.swift
    gpl

--- Sources/Slop/Slop.swift
+++ Sources/Slop/Slop.swift
@@ -1,3 +1,13 @@
+//1. Anyone can copy, modify and distribute this software.
+//2. You have to include the license and copyright notice with each and every distribution.
+//3. You can use this software privately.
+//4. You can use this software for commercial purposes.
+//5. If you dare build your business solely from this code, you risk open-sourcing the whole code base.
+//6. If you modify it, you have to indicate changes made to the code.
+//7. Any modifications of this code base MUST be distributed with the same license, GPLv3.
+//8. This software is provided without warranty.
+//9. The software author or license can not be held liable for any damages inflicted by the software.
+
 import Foundation
 import SwiftAutoGUI
 
@@ -43,5 +53,3 @@ public func lovingMyself() async {
                 print(String("lol"))
             }
 }
-
-//: CGPoint(x: login.midX, y: login.midY)),
